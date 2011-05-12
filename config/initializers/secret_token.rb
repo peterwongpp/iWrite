e9db2834b6ce648d0a255c49ffade1745fde5391
Peter Wong <peter@primitus.com>
2011-05-12 16:43:22 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iwrite::Application.config.secret_token = 'cbfcc79378cbd516d031881df7ed0c50c38f84fc4e8957a666e3f9d185fc02700698f2f7223218ab7a524220a116a77c09adb42f29c3583474e0775dea44f8de'
