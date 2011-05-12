1a234498bc55febd0836267463daf137e89e7ed2
Peter Wong <peter@primitus.com>
2011-05-12 17:12:12 +0800

    Added scaffold for Book and some seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Iwrite::Application.routes.draw do
+  resources :books
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  
+  root :to => 'books#index'
+    
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+module MyRandom
+  def self.boolean
+    ((rand * 2).to_i == 1) ? true : false
+  end
+  
+  def self.date
+    (rand * 300).to_i.days.ago
+  end
+end
+
+15.times do |n|
+  published = MyRandom.boolean
+  published_at = published ? MyRandom.date : nil
+  
+  Book.create({
+    :title => "Fake Book #{n}",
+    :content => "A longer fake content.\n\nWith line breaks XD\n\nThis is fake book #{n}",
+    :published_at => published_at
+  })
+end
